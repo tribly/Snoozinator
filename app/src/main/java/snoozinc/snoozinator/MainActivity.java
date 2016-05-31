f7fa9d7a1e5f5a60a1eb4c6e5ad7304a16cc185b
Hrvoje Hodak <dev@tribly.de>
2016-05-31 16:52:51 +0200

    Implement picking a time for the alarm

--- app/src/main/java/snoozinc/snoozinator/MainActivity.java
+++ app/src/main/java/snoozinc/snoozinator/MainActivity.java
@@ -1,15 +1,17 @@
 package snoozinc.snoozinator;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minuteOfDay) {
+        /*
+        * Make sure the time is only set once. Dialog has a bug giving a value, when closed without
+        * hitting the 'OK' button
+        * */
+        if (view.isShown()) {
+            TextView textView = (TextView) findViewById(R.id.alarm_time);
+            /*
+            * Format the hour and minute so that we have leading zeroes
+            * */
+            String hour = String.format("%02d", hourOfDay);
+            String minute = String.format("%02d", minuteOfDay);
+            textView.setText(hour + ":" + minute);
+        }
+    }
+
+    public void showTimePicker(View v) {
+        TimePickerFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
     }
 
     @Override
--- app/src/main/java/snoozinc/snoozinator/TimePickerFragment.java
+++ app/src/main/java/snoozinc/snoozinator/TimePickerFragment.java
@@ -1,7 +1,40 @@
 package snoozinc.snoozinator;
 
-/**
- * Created by heinz on 5/31/16.
- */
-public class TimePickerFragment {
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import java.util.Calendar;
+
+
+public class TimePickerFragment extends DialogFragment {
+
+    private Activity mActivity;
+    private TimePickerDialog.OnTimeSetListener mListener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mActivity = activity;
+
+        try {
+            mListener = (TimePickerDialog.OnTimeSetListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement onTimeSetListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(mActivity, mListener, hour, minute,
+                                    DateFormat.is24HourFormat(mActivity));
+    }
+
+
 }
