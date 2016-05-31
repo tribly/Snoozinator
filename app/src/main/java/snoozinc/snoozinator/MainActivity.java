50ad3a27ec560d5257562415ebb5e731cc951f64
nodlek <luis.rodriguez-diesner@student.uni-tuebingen.de>
2016-05-31 15:45:14 +0200

    Implement
    implement basic scrollview, elements can be added and deleted

--- app/src/main/java/snoozinc/snoozinator/MainActivity.java
+++ app/src/main/java/snoozinc/snoozinator/MainActivity.java
@@ -1,16 +1,33 @@
 package snoozinc.snoozinator;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences allAlarms;
+
+    TableLayout alarmTableScrollView;
+
+    Button addAlarmButton;
+    Button deleteAllAlarmsButton;
+
+    int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +35,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        alarmTableScrollView = (TableLayout) findViewById(R.id.alarmTableScrollView);
+
+        addAlarmButton = (Button) findViewById(R.id.addAlarmButton);
+        deleteAllAlarmsButton = (Button)  findViewById(R.id.deleteAllAlarmsButton);
+
+        id = 0;
+
+        addAlarmButton.setOnClickListener(addAlarmButtonListener);
+        deleteAllAlarmsButton.setOnClickListener(deleteAllAlarmsButtonListener);
+
     }
+
+    private void insertAlarmInScrollView(){
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View newAlarm = inflater.inflate(R.layout.alarm_item_layout, null);
+
+        Button deleteAlarmButton = (Button) newAlarm.findViewById(R.id.deleteAlarmButton);
+        deleteAlarmButton.setOnClickListener(deleteAlarmButtonListener);
+
+        alarmTableScrollView.addView(newAlarm, id++);
+
+    }
+
+    public View.OnClickListener addAlarmButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            insertAlarmInScrollView();
+        }
+    };
+
+    public View.OnClickListener deleteAllAlarmsButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            alarmTableScrollView.removeAllViews();
+            id = 0;
+        }
+    };
+
+    public View.OnClickListener deleteAlarmButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            alarmTableScrollView.removeViewAt(--id);
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,7 +107,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void bla(View v){
-        startActivity(new Intent(MainActivity.this, TestActivity.class));
-    }
 }
